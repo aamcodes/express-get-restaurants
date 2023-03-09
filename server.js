47ae00f9be5b77cfc4efc538bfc85b9f71aa14a4
aamcodes <aaronmendezcodes@gmail.com>
2023-03-09 00:39:50 -0500

    added crud functionality to the server routes to create, update and delete instances in the database

--- server.js
+++ server.js
@@ -37,6 +37,70 @@ app.get('/restaurants/:id', async (req, res) => {
 	}
 });
 
+app.post('/restaurants', async (req, res) => {
+	try {
+		let { name, location, cuisine } = req.body;
+		if (!name || !location || !cuisine) {
+			res.status(400).json({ message: 'All fields are required' });
+		} else {
+			await Restaurant.create({ name, location, cuisine })
+				.then((restaurant) => {
+					res.status(201).json(restaurant);
+				})
+				.catch((err) => {
+					res.status(500).json({
+						message: 'Database Internal Error',
+					});
+				});
+		}
+	} catch (err) {
+		res.status(500).json({ message: 'Server Internal Error' });
+	}
+});
+
+app.put('/restaurants/:id', async (req, res) => {
+	try {
+		let { id } = req.params;
+		let { name, location, cuisine } = req.body;
+		if (!name || !location || !cuisine) {
+			res.status(400).json({ message: 'All fields are required' });
+		}
+		let restaurant;
+		await Restaurant.findByPk(id)
+			.then((existingRestaurant) => {
+				restaurant = existingRestaurant;
+				restaurant.set({
+					name: name,
+					location: location,
+					cuisine: cuisine,
+				});
+				restaurant.save();
+				res.status(201).json(restaurant);
+			})
+			.catch((err) => {
+				res.status(404).json({ message: 'Restaurant not found' });
+			});
+	} catch (err) {
+		res.status(500).json({ message: 'Server Internal Error' });
+	}
+});
+
+app.delete('/restaurants/:id', async (req, res) => {
+	try {
+		let { id } = req.params;
+		await Restaurant.findByPk(id)
+			.then((foundRestaurant) => {
+				foundRestaurant.destroy();
+				res.status(201).json({ removed: foundRestaurant });
+			})
+			.catch((err) => {
+				res.status(404).json({ message: 'No restaurant found' });
+			});
+	} catch (err) {
+		res.status(500).json({ message: 'Server Internal Error' });
+	}
+});
+
 app.listen(port, () => {
 	sequelize.sync();
 	console.log('Your server is listening on port ' + port);
